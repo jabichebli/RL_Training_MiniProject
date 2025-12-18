38f1747bf2ab4651670e0a14bef057626d89fbb1
at1047 <andrewhytai@gmail.com>
2025-12-17 21:22:02 -0800

    increase tracking joint reward weight, remove termination conditions for now in train

--- src/mjlab/tasks/tracking/tracking_env_cfg.py
+++ src/mjlab/tasks/tracking/tracking_env_cfg.py
@@ -282,7 +282,7 @@ def create_tracking_env_cfg(
     ),
     "motion_joint_pos": RewardTermCfg(
       func=mdp.motion_joint_position_error_exp,
-      weight=1.0,
+      weight=5.0,
       params={"command_name": "motion", "std": 0.5},
     ),
     "action_rate_l2": RewardTermCfg(func=mdp.action_rate_l2, weight=-1e-1),
